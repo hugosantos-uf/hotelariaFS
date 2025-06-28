ec8e557bc3bd941f44009edbc1dbfdb5d498582c
Hugo Santos <hugosantos.ufg@gmail.com>
2025-06-28 07:36:40 -0300

    User entity

---
+++ src/main/java/com/hs/hotelaria/entity/User.java
@@ -0,0 +1,65 @@
+package com.hs.hotelaria.entity;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email deve ser preenchido")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Nome deve ser preenchido")
+    private String name;
+
+    @NotBlank(message = "Número de telefone deve ser preenchido")
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
